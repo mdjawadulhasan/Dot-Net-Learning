64a539a756bfbe11b98428d360acae08ccbfae30
mdjawadulhasan <jhjawad111@gmail.com>
2022-09-04 12:43:04 +0600

    Add project files.

---
+++ BismillahApp1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BismillahApp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            return View("COMP");
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+
+   
+}
