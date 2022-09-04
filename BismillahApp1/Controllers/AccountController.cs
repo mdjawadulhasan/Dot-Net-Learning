414c2f720a4697afb2f0f4cbd9f160bdde4087c1
mdjawadulhasan <jhjawad111@gmail.com>
2022-09-04 17:19:24 +0600

    RedirectToRoute

---
+++ BismillahApp1/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BismillahApp1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string uname, string pass)
+        {
+            if (uname == "ishan" && pass == "123")
+            {
+                return RedirectToAction("Dashboard", "Admin");
+            }
+            else
+            {
+                return RedirectToAction("InvalidLogin");
+            }
+           
+        }
+
+        public ActionResult InvalidLogin(string uname, string pass)
+        {
+            return View();
+        }
+    }
+}
