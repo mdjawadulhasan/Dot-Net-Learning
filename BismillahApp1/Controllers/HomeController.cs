846a6f63f4a6f152471b960da33e294e24e91d09
mdjawadulhasan <jhjawad111@gmail.com>
2022-09-04 14:36:15 +0600

    Redirect result

--- BismillahApp1/Controllers/HomeController.cs
+++ BismillahApp1/Controllers/HomeController.cs
@@ -24,36 +24,11 @@ namespace BismillahApp1.Controllers
             return View();
         }
 
-        /*public ActionResult GetStudent(int id)
+        public ActionResult StudentPage(int id)
         {
-            var students = new[] {
-            new { Id = 1, FirstName = "James", LastName = "Bond" },
-            new { Id = 2, FirstName = "Steve", LastName = "Jobs" },
-            new { Id = 3, FirstName = "Bill", LastName = "Gates" }
-            };
-
-            string studentname=null;
-            foreach (var items in students)
-            {
-                if (items.Id == id)
-                {
-                    studentname = items.FirstName;
-                }
-            }
-
-            return Content(studentname, "text/plain");
-
-        }*/
-
-
-
-        public ActionResult GetStudentResultSlip (int id)
-        {
-            //File need to be kept inside the project folder
-            string filename = "~/Payslip" + id + ".pdf";
-            return File(filename,"application/pdf");
+            string stlinkedinid = "https://www.linkedin.com/in/mdjawadulhasan/" + id;
+            return Redirect(stlinkedinid);
         }
-
 
     }
 
