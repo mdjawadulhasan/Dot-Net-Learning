611812b14338f37e87363b6e32df315868dc0ab4
mdjawadulhasan <jhjawad111@gmail.com>
2022-09-06 15:30:33 +0600

    URL routing id parameter optional

---
+++ BismillahApp1/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BismillahApp1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            var producrs = new[]
+            {
+                new {productid=1,productname="xiomi",cost=23000},
+                new {productid=2,productname="Apple",cost=123000}
+            };
+
+            if (id == null)
+            {
+                return Content("Kisu ekta to den vai !");
+            }
+            else
+            {
+                string prodname = "";
+                foreach (var items in producrs)
+                {
+                    if (items.productid == id)
+                    {
+                        prodname = items.productname;
+                    }
+                }
+                return Content(prodname);
+            }
+        }
+    }
+}
