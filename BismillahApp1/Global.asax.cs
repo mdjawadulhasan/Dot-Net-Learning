11dbe51fb0ccfcaed9d51180ee69a8f263f1d203
mdjawadulhasan <jhjawad111@gmail.com>
2022-09-07 12:29:55 +0600

    Custom Binder Added

--- BismillahApp1/Global.asax.cs
+++ BismillahApp1/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using BismillahApp1.Models;
 
 namespace BismillahApp1
 {
@@ -13,6 +14,7 @@ namespace BismillahApp1
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ModelBinders.Binders.Add(typeof(Student),new CustomBinder());
         }
     }
 }
