d2ebcaf6cc645da823c74c67a261aec2406b12c0
mdjawadulhasan <jhjawad111@gmail.com>
2022-09-21 23:07:44 +0600

    Ready For configuring roles

---
+++ BismillahApp1/IdentityMigrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace BismillahApp1.IdentityMigrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BismillahApp1.Identity.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"IdentityMigrations";
+        }
+
+        protected override void Seed(BismillahApp1.Identity.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
