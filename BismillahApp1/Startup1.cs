46fe5dd77a2fd92e013474f81d15142a70e4c5db
mdjawadulhasan <jhjawad111@gmail.com>
2022-09-21 21:55:30 +0600

    Installed all three packages

---
+++ BismillahApp1/Startup1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+
+[assembly: OwinStartup(typeof(BismillahApp1.Startup1))]
+
+namespace BismillahApp1
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        
+        }
+    }
+}
