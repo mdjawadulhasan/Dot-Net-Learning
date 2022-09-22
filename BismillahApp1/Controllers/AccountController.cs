01edd272a36b0771be65f717ecb07806150604f6
mdjawadulhasan <jhjawad111@gmail.com>
2022-09-22 23:52:41 +0600

    Registration is Done

---
+++ BismillahApp1/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BismillahApp1.Models;
+using BismillahApp1.ViewModels;
+using BismillahApp1.Identity;
+using System.Web.Helpers;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace BismillahApp1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel rvm)
+        {
+            var appDbContext = new ApplicationDbContext();
+            var userStore = new ApplicationUserStore(appDbContext);
+            var userManager = new ApplicationUserManager(userStore);
+            var passwordHash = Crypto.HashPassword(rvm.Password);
+            var user = new ApplicationUser() { Email = rvm.Email, UserName = rvm.Username, PasswordHash = passwordHash, City = rvm.City, Country = rvm.Country, Birthday = rvm.DateOfBirth, Address = rvm.Address, PhoneNumber = rvm.Mobile };
+            IdentityResult result = userManager.Create(user);
+
+            if (result.Succeeded)
+            {
+                //role
+                userManager.AddToRole(user.Id, "Customer");
+
+                //login
+                var authenticationManager = HttpContext.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                authenticationManager.SignIn(new AuthenticationProperties(), userIdentity);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
